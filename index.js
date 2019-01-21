4b5b68228ae1e4a19c1c04fbd5e8d21c17edc3ad
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 03:49:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var new_object = Object.assign({}, object)
-  delete new_object.key
+Object.assign({}, object { key})
   return object
 }
 
